0aa6fd4f450b1705fff07170dfbac68b17ed1420
shubham singh <shubhmsing@gmail.com>
2015-11-07 13:30:32 +0530

    ranklist added

--- ranklist.php
+++ ranklist.php
@@ -32,6 +32,35 @@
   <body>
   
   <div class="container">
+		      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Questionnaire</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li ><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Questions <span class="sr-only">(current)</span></a></li>
+              <li class="active"><a href="ranklist.php">Ranklist</a></li>
+              <li><a href="logout.php">Log Out</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+
+      
+      <nav>
+	  
 		<div class="jumbotron">
 		<h1 align="center">Ranklist</h1>
 			<form class="navbar-form navbar-right" role="search" method="post">
