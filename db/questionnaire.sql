e24b461d64827abdae599c4aac31dd879c48b251
kripashanker <iamkripashanker@gmail.com>
2015-11-08 21:28:20 +0530

    created table feedback

--- db/questionnaire.sql
+++ db/questionnaire.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 4.4.14.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 07, 2015 at 08:53 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Generation Time: Nov 08, 2015 at 03:57 PM
+-- Server version: 10.0.21-MariaDB
+-- PHP Version: 5.6.13
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `questionnaire`
@@ -46,8 +46,7 @@ CREATE TABLE IF NOT EXISTS `answer` (
   `q16` varchar(10) NOT NULL,
   `q17` varchar(10) NOT NULL,
   `q18` varchar(10) NOT NULL,
-  `q19` varchar(10) NOT NULL,
-  PRIMARY KEY (`rollno`)
+  `q19` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -64,7 +63,32 @@ INSERT INTO `answer` (`rollno`, `q1`, `q2`, `q3`, `q4`, `q5`, `q6`, `q7`, `q8`, 
 (532, 'opt2', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
 (789, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
 (1311010007, 'opt3', '', 'opt4', 'opt1', '', 'opt3', '', 'opt2', '', '', '', '', '', '', '', '', '', '', ''),
-(1311010008, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+(1311010008, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+(1311010024, '', 'opt1', 'opt4', 'opt4', 'opt4', 'opt3', 'opt4', 'opt1', '', '', '', '', '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `user` int(11) NOT NULL,
+  `liketype` varchar(1000) NOT NULL,
+  `prob` varchar(1000) NOT NULL,
+  `problems` varchar(1000) NOT NULL,
+  `questions` varchar(1000) NOT NULL,
+  `experience` varchar(1000) NOT NULL,
+  `improve` varchar(1000) NOT NULL,
+  `start` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`user`, `liketype`, `prob`, `problems`, `questions`, `experience`, `improve`, `start`) VALUES
+(1311010024, '', '', '', '', '', '', 'finish');
 
 -- --------------------------------------------------------
 
@@ -73,15 +97,14 @@ INSERT INTO `answer` (`rollno`, `q1`, `q2`, `q3`, `q4`, `q5`, `q6`, `q7`, `q8`, 
 --
 
 CREATE TABLE IF NOT EXISTS `question` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `question` varchar(1000) NOT NULL,
   `opt1` varchar(1000) NOT NULL,
   `opt2` varchar(1000) NOT NULL,
   `opt3` varchar(1000) NOT NULL,
   `opt4` varchar(1000) NOT NULL,
-  `optcr` varchar(1000) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+  `optcr` varchar(1000) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `question`
@@ -104,24 +127,23 @@ INSERT INTO `question` (`id`, `question`, `opt1`, `opt2`, `opt3`, `opt4`, `optcr
 --
 
 CREATE TABLE IF NOT EXISTS `ranklist` (
-  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `id` int(100) NOT NULL,
   `rollno` int(100) NOT NULL,
   `rank` int(100) NOT NULL,
   `correct` int(100) NOT NULL,
   `wrong` int(100) NOT NULL,
-  `total` int(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `rollno` (`rollno`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `total` int(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ranklist`
 --
 
 INSERT INTO `ranklist` (`id`, `rollno`, `rank`, `correct`, `wrong`, `total`) VALUES
-(1, 1311010007, 1, 2, 6, -4),
-(2, 1311010008, 2, 0, 8, -8),
-(3, 789, 2, 0, 8, -8);
+(1, 1311010007, 2, 2, 6, -4),
+(2, 1311010008, 3, 0, 8, -8),
+(3, 789, 3, 0, 8, -8),
+(4, 1311010024, 1, 4, 4, 0);
 
 -- --------------------------------------------------------
 
@@ -140,8 +162,7 @@ CREATE TABLE IF NOT EXISTS `registration` (
   `contact` int(11) NOT NULL,
   `facebookid` varchar(1000) NOT NULL,
   `address` varchar(1000) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  PRIMARY KEY (`rollno`)
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -152,8 +173,58 @@ INSERT INTO `registration` (`name`, `branch`, `year`, `university`, `college`, `
 ('hbhb', 'CSE', 1, 'kbkhbkh', 'k k khjb', 532, 'kbkh@hoh', 123, 'kbub', 'biub', '532'),
 ('ajay532', 'CSE', 1, 'jhvjhv', 'hvhjhb', 789, 'kbk@jnjn', 5644, 'nnon', 'non', '789'),
 ('jnljn', 'CSE', 1, 'lknkn', 'lknlk', 1234, 'kln@df', 535, 'knlkn', 'klnlknkl', '1234'),
-('AJAY PANDEY', 'CSE', 1, 'UPTU', 'IERT', 1311010007, 'ajaypandey1996@gmail.com', 1234565161, '3513513', '138/15 Ram Priya Road', 'pass');
+('AJAY PANDEY', 'CSE', 1, 'UPTU', 'IERT', 1311010007, 'ajaypandey1996@gmail.com', 1234565161, '3513513', '138/15 Ram Priya Road', 'pass'),
+('kripa', 'CSE', 1, 'AB', 'CD', 1311010024, 'abc@gmail.com', 12345, 'xcvbn', 'sdfghjk', '123');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answer`
+--
+ALTER TABLE `answer`
+  ADD PRIMARY KEY (`rollno`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`user`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ranklist`
+--
+ALTER TABLE `ranklist`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `rollno` (`rollno`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`rollno`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `ranklist`
+--
+ALTER TABLE `ranklist`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
