322aba1a96b0b439a559faba77e39f1f56879ff2
shubham singh <shubhmsing@gmail.com>
2017-02-17 14:19:22 +0530

    change in schema
    timer table added

--- db/questionnaire.sql
+++ db/questionnaire.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14.1
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 08, 2015 at 03:57 PM
--- Server version: 10.0.21-MariaDB
--- PHP Version: 5.6.13
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2017 at 09:48 AM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `answer`
 --
 
-CREATE TABLE IF NOT EXISTS `answer` (
+CREATE TABLE `answer` (
   `rollno` int(11) NOT NULL,
   `q1` varchar(10) NOT NULL,
   `q2` varchar(10) NOT NULL,
@@ -49,30 +49,13 @@ CREATE TABLE IF NOT EXISTS `answer` (
   `q19` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `answer`
---
-
-INSERT INTO `answer` (`rollno`, `q1`, `q2`, `q3`, `q4`, `q5`, `q6`, `q7`, `q8`, `q9`, `q10`, `q11`, `q12`, `q13`, `q14`, `q15`, `q16`, `q17`, `q18`, `q19`) VALUES
-(0, '', 'opt4', '', '', '', 'opt3', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(1, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(2, '', '', '', 'opt3', '', 'opt2', '', 'opt3', '', '', '', '', '', '', '', '', '', '', ''),
-(4, '', '', '', '', '', 'opt2', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(5, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(123, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(532, 'opt2', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(789, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(1311010007, 'opt3', '', 'opt4', 'opt1', '', 'opt3', '', 'opt2', '', '', '', '', '', '', '', '', '', '', ''),
-(1311010008, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-(1311010024, '', 'opt1', 'opt4', 'opt4', 'opt4', 'opt3', 'opt4', 'opt1', '', '', '', '', '', '', '', '', '', '', '');
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `feedback`
 --
 
-CREATE TABLE IF NOT EXISTS `feedback` (
+CREATE TABLE `feedback` (
   `user` int(11) NOT NULL,
   `liketype` varchar(1000) NOT NULL,
   `prob` varchar(1000) NOT NULL,
@@ -88,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 --
 
 INSERT INTO `feedback` (`user`, `liketype`, `prob`, `problems`, `questions`, `experience`, `improve`, `start`) VALUES
-(1311010024, '', '', '', '', '', '', 'finish');
+(1, '', '', '', '', '', '', 'finish');
 
 -- --------------------------------------------------------
 
@@ -96,7 +79,7 @@ INSERT INTO `feedback` (`user`, `liketype`, `prob`, `problems`, `questions`, `ex
 -- Table structure for table `question`
 --
 
-CREATE TABLE IF NOT EXISTS `question` (
+CREATE TABLE `question` (
   `id` int(11) NOT NULL,
   `question` varchar(1000) NOT NULL,
   `opt1` varchar(1000) NOT NULL,
@@ -104,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   `opt3` varchar(1000) NOT NULL,
   `opt4` varchar(1000) NOT NULL,
   `optcr` varchar(1000) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `question`
@@ -118,7 +101,8 @@ INSERT INTO `question` (`id`, `question`, `opt1`, `opt2`, `opt3`, `opt4`, `optcr
 (5, 'who is yur favourite actor ?', 'varun dhavan', 'shahrukh khan', 'salman khan', 'amir khan', 'opt3'),
 (6, 'who is yur favourite actor ?', 'varun dhavan', 'shahrukh khan', 'salman khan', 'amir khan', 'opt3'),
 (7, 'who is yur favourite actor ?', 'varun dhavan', 'shahrukh khan', 'salman khan', 'amir khan', 'opt3'),
-(8, 'Shubham', 'lnl', 'jjk', 'j jk k', 'jj j', 'opt1');
+(8, 'Shubham', 'lnl', 'jjk', 'j jk k', 'jj j', 'opt1'),
+(9, 'dsfioejioeg', 'a', 'a', 's', 's', 'opt1');
 
 -- --------------------------------------------------------
 
@@ -126,24 +110,14 @@ INSERT INTO `question` (`id`, `question`, `opt1`, `opt2`, `opt3`, `opt4`, `optcr
 -- Table structure for table `ranklist`
 --
 
-CREATE TABLE IF NOT EXISTS `ranklist` (
+CREATE TABLE `ranklist` (
   `id` int(100) NOT NULL,
   `rollno` int(100) NOT NULL,
   `rank` int(100) NOT NULL,
   `correct` int(100) NOT NULL,
   `wrong` int(100) NOT NULL,
   `total` int(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ranklist`
---
-
-INSERT INTO `ranklist` (`id`, `rollno`, `rank`, `correct`, `wrong`, `total`) VALUES
-(1, 1311010007, 2, 2, 6, -4),
-(2, 1311010008, 3, 0, 8, -8),
-(3, 789, 3, 0, 8, -8),
-(4, 1311010024, 1, 4, 4, 0);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -151,7 +125,7 @@ INSERT INTO `ranklist` (`id`, `rollno`, `rank`, `correct`, `wrong`, `total`) VAL
 -- Table structure for table `registration`
 --
 
-CREATE TABLE IF NOT EXISTS `registration` (
+CREATE TABLE `registration` (
   `name` varchar(100) NOT NULL,
   `branch` char(10) NOT NULL,
   `year` int(11) NOT NULL,
@@ -165,16 +139,24 @@ CREATE TABLE IF NOT EXISTS `registration` (
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `registration`
+-- Table structure for table `timer`
 --
 
-INSERT INTO `registration` (`name`, `branch`, `year`, `university`, `college`, `rollno`, `email`, `contact`, `facebookid`, `address`, `password`) VALUES
-('hbhb', 'CSE', 1, 'kbkhbkh', 'k k khjb', 532, 'kbkh@hoh', 123, 'kbub', 'biub', '532'),
-('ajay532', 'CSE', 1, 'jhvjhv', 'hvhjhb', 789, 'kbk@jnjn', 5644, 'nnon', 'non', '789'),
-('jnljn', 'CSE', 1, 'lknkn', 'lknlk', 1234, 'kln@df', 535, 'knlkn', 'klnlknkl', '1234'),
-('AJAY PANDEY', 'CSE', 1, 'UPTU', 'IERT', 1311010007, 'ajaypandey1996@gmail.com', 1234565161, '3513513', '138/15 Ram Priya Road', 'pass'),
-('kripa', 'CSE', 1, 'AB', 'CD', 1311010024, 'abc@gmail.com', 12345, 'xcvbn', 'sdfghjk', '123');
+CREATE TABLE `timer` (
+  `rollno` int(11) NOT NULL,
+  `start_time` varchar(100) NOT NULL,
+  `end_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `timer`
+--
+
+INSERT INTO `timer` (`rollno`, `start_time`, `end_time`) VALUES
+(1, '2017-02-17 14:17:36', '2017-02-17 14:19:36');
 
 --
 -- Indexes for dumped tables
@@ -212,6 +194,12 @@ ALTER TABLE `registration`
   ADD PRIMARY KEY (`rollno`);
 
 --
+-- Indexes for table `timer`
+--
+ALTER TABLE `timer`
+  ADD PRIMARY KEY (`rollno`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -219,12 +207,12 @@ ALTER TABLE `registration`
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `ranklist`
 --
 ALTER TABLE `ranklist`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
